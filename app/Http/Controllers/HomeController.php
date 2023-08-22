b40cf65aee23b3c8752e8dea71a4f5ab75e3791b
houssine <houssineagha963@gmail.com>
2023-08-22 13:37:02 +0300

    add notes in home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Note;
 class HomeController extends Controller
 {
     public function index(Request $request)
     {
-        return view('index');
+        $note= Note::where('user_id', 1)->orderByDesc('created_at')->paginate(9);
+        return view('index',compact('note'));
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,14 +3,37 @@
 @section('title','الرئيسية')
 
 @section('style')
+<style>
 
+</style>
 @endsection
 
 @section('content')
 
-<div class="alert alert-primary" role="alert">
-    A simple primary alert—check it out!
-  </div>
+<main class="backround" >
+    <div class="container">
+        <div class="row" style="margin-top: 50px;margin-bottom:50px;">
+    @foreach ($note as $item)
+        <div class="col-4" style="margin-bottom:15px; ">
+            <div class="card" style="width: 18rem;">
+                @isset($item->image)
+                <img src="{{asset($item->image)}}" class="card-img-top" alt="..." height="150px" width="150px;">
+                @endisset
+                @empty($item->image)
+                <img src="{{asset('image/defult.jpg')}}" class="card-img-top" alt="..." height="150px" width="150px;">
+                @endempty
+                <div class="card-body">
+                    <p class="card-text">{{$item->description}}</p>
+                </div>
+            </div>
+        </div>
+    @endforeach
+    <div class="paginate" style="margin-top: 15px; padding-left:40%;">
+        {{$note->links()}}
+    </div>
+        </div>
+    </div>
+</main>
 
 @section('script')
 
